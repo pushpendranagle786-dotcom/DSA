c8a5d38cea6bcf092e970b6046d84c3543b7bbca
pushpendranagle786-dotcom <pushpendranagle786@gmail.com>
2026-09-13 18:43:20 +0530

    Two Sum Problem
    Fix loop condition to prevent out-of-bounds access.

--- Array/Two_Sum.cpp
+++ Array/Two_Sum.cpp
@@ -1,1 +1,21 @@
+//Problem: Two Sum
+//LeetCode:1
+//Time:O(n)
+//Space:O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0; i<=nums.size(); i++){
+            int need=target-nums[i];
+            if(mp.find(need)!=mp.end()){
+                return{mp[need],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return ans;
+    }
+};
 
