d9f1939bd2a68bc90d1d9d55b1e757e96a267ab1
pushpendranagle786-dotcom <pushpendranagle786@gmail.com>
2026-09-15 11:20:32 +0530

    Add Online Stock Span brute force solution
    Implemented the StockSpanner class using a brute force approach to calculate the stock span for each price input.

--- Stack/Online-Stock-Span/Brute_Force.cpp
+++ Stack/Online-Stock-Span/Brute_Force.cpp
@@ -1,1 +1,31 @@
-
+// Problem: Online Stock Span
+// LeetCode: 901
+// Approach: Brute Force
+// Time: O(n^2)
+// Space: O(n)
+class StockSpanner {
+    vector<int>Price;
+    int count=1;
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        count=1;
+        if(Price.empty()){
+            Price.push_back(price);
+            return count;
+        }
+        for(int i=Price.size()-1; i>=0; i--){
+            if(price>=Price[i]){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        Price.push_back(price);
+        return count;
+    }
+};
